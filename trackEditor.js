02f6647a4703430d63315274ed6d5c7c88ed2cd7
Gustavo Figueroa <gcfiguer@uci.edu>
2016-05-21 14:47:05 -0700

    Comments
    Added comments to help Bob distinguish the two button’s functions.

--- trackEditor.js
+++ trackEditor.js
@@ -2,6 +2,7 @@
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var source;
 
+//chooseFile is for file upload.
 function chooseFile()
 {
 	console.log("Sup kid");
@@ -26,6 +27,8 @@ function chooseFile()
 
 }
 
+
+//Called when file is selected in file browser.
 document.getElementById('fileInput').onchange = function () {
   console.log('Selected file: ' + this.value);
   var file = URL.createObjectURL(this.files[0]);
