4bf814c38ce26d43f37c81ccb0e203dac4a75095
efrenaguilar95 <efrenaguilar95@hotmail.com>
2016-05-29 00:09:35 -0700

    Added gain and slider
    All that needs to be done now is connect the value of slider to the gain
    node to change it appropriately.

--- trackEditor.js
+++ trackEditor.js
@@ -1,5 +1,7 @@
 //var finalFile;
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var gainNode = audioCtx.createGain();
+gainNode.connect(audioCtx.destination);
 var source;
 var file;
 var request;
@@ -10,19 +12,22 @@ var audioBits;
 function chooseFile()
 {
 	request.onload = function() {
+    console.log("hi3");
     var audioData = request.response;
 
     audioBuffer = audioCtx.decodeAudioData(audioData, function(buffer) {
         source.buffer = buffer;
 
-        source.connect(audioCtx.destination);
+        source.connect(gainNode);
         source.loop = true;
       },
 
       function(e){"Error with decoding audio data" + e.err});
-
+    //console.log(source);
+    //console.log(audioBuffer, "jkdsjwdka");
   }
   request.send();
+
 
 }
 
@@ -37,7 +42,6 @@ document.getElementById('fileInput').onchange = function () {
   request = new XMLHttpRequest();
   request.open('GET', file, true);
   request.responseType = 'arraybuffer';
-
 };
 
 function playAudio()
@@ -45,13 +49,27 @@ function playAudio()
 	source.start();
 }
 
+function stopAudio()
+{
+  gainNode.gain.value = 10;
+  //source.stop();
+}
+
+function toggleGain()
+{
+
+}
+
+
 function setupDownload()
 {
   audioBits = source.buffer.getChannelData(0);
+  console.log(audioBits.buffer);
+  var blob = new Blob();
   //console.log(audioBits);
-  //var url = (window.URL || window.webkitURL).createObjectURL(file);
-  var link = document.getElementById("download");
-  link.href = file;
-  link.download = "coolAudio.wav";
+  //var url = (window.URL || window.webkitURL).createObjectURL(blob);
+  //var link = document.getElementById("download");
+  //link.href = url;
+  //link.download = "coolAudio.wav";
 }
 
