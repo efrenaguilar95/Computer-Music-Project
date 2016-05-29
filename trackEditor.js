28d8500f5ff2072510a7b10841673b26633d51f7
efrenaguilar95 <efrenaguilar95@hotmail.com>
2016-05-28 19:29:54 -0700

    Small fixes and progress towards correct downloads
    Code now only plays music from a buffer source node. We are also now
    able to get the actual PCM data of the audio file to be later saved into
    a new audio file.

--- trackEditor.js
+++ trackEditor.js
@@ -2,19 +2,17 @@
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 var source;
 var file;
+var request;
+var audioBuffer;
+var audioBits;
 
 //chooseFile is for file upload.
 function chooseFile()
 {
-	console.log("Sup kid");
-	source = audioCtx.createBufferSource();
-	var request = new XMLHttpRequest();
-	request.open("GET", file, true);
-	request.responseType = "arraybuffer";
 	request.onload = function() {
     var audioData = request.response;
 
-    audioCtx.decodeAudioData(audioData, function(buffer) {
+    audioBuffer = audioCtx.decodeAudioData(audioData, function(buffer) {
         source.buffer = buffer;
 
         source.connect(audioCtx.destination);
@@ -36,21 +34,21 @@ document.getElementById('fileInput').onchange = function () {
   //finalFile = file;
   audioPlayer.src = file;
   source = audioCtx.createBufferSource();
-  var request = new XMLHttpRequest();
+  request = new XMLHttpRequest();
   request.open('GET', file, true);
   request.responseType = 'arraybuffer';
-  source.start(0);
 
-  //audioPlayer.play();
 };
 
 function playAudio()
 {
-	audioPlayer.play(0);
+	source.start();
 }
 
 function setupDownload()
 {
+  audioBits = source.buffer.getChannelData(0);
+  //console.log(audioBits);
   //var url = (window.URL || window.webkitURL).createObjectURL(file);
   var link = document.getElementById("download");
   link.href = file;
